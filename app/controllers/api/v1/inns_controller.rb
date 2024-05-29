091cb5eed0cb7774426256da0935aa1b70851bd0
Mariano <onairam17@gmail.com>
2024-05-29 11:03:46 -0400

    fim

--- app/controllers/api/v1/inns_controller.rb
+++ app/controllers/api/v1/inns_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::InnsController < ActionController::API
   def show
     inn = Inn.all.where("inns.registration_number LIKE? ", params[:id] )
+    n_rooms = inn.inn_rooms
 
-    render status: 200, json: inn.as_json
+    detalhes=[name: inn.name, registration_number: inn.registration_number, description: inn.description,
+                full_address: full_address , n_rooms: n_rooms]
+    render status: 200, json: detalhes.as_json
   end
 end
